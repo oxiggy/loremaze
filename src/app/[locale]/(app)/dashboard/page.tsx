efa45aaefd4cb0726c0d62d2abbc38f236a109a5
Ada Void <oxiggy@oxiggy.com>
2025-04-11 10:56:43 +0400

    feat: add supabase scripts, upd biome config, gen types

--- src/app/[locale]/(app)/dashboard/page.tsx
+++ src/app/[locale]/(app)/dashboard/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-	return <div>dashboard</div>
+import { createClient } from '@/lib/supabase'
+
+export default async function Page() {
+	const client = await createClient()
+
+	const { data, error } = await client.from('test').select(`
+		id,
+		titleEn,
+		contentEn,
+		test_question (
+			id,
+			question (
+				id,
+				contentEn
+			)
+		)
+	`)
+
+	if (error) {
+		throw error
+	}
+
+	return (
+		<div>
+			<div>dashboard</div>
+			<pre>{JSON.stringify(data, null, 2)}</pre>
+		</div>
+	)
 }
