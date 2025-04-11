f26d9162cf996c6759b1b8a22a45b5208ba3fa67
Ada Void <oxiggy@oxiggy.com>
2025-04-11 22:27:44 +0400

    db: start migration

---
+++ supabase/migrations/20250411151557_create_profiles_table.sql
@@ -0,0 +1,126 @@
+/*create table "public"."profiles" (
+    "id" uuid not null,
+    "updatedAt" timestamp with time zone,
+    "username" text,
+    "fullName" text,
+    "avatarUrl" text,
+    "website" text
+);
+
+
+alter table "public"."profiles" enable row level security;
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+CREATE UNIQUE INDEX profiles_username_key ON public.profiles USING btree (username);
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+alter table "public"."profiles" add constraint "profiles_username_key" UNIQUE using index "profiles_username_key";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO ''
+AS $function$
+begin
+  insert into public.profiles (id, fullName, avatarUrl)
+  values (new.id, coalesce(new.raw_user_meta_data->>'fullName', ''), coalesce(new.raw_user_meta_data->>'avatarUrl', ''));
+  return new;
+end;
+$function$
+;
+
+CREATE TRIGGER on_auth_user_created
+    AFTER INSERT ON auth.users
+    FOR EACH ROW
+    EXECUTE PROCEDURE public.handle_new_user();
+
+INSERT INTO storage.buckets (id, name)
+VALUES ('avatars', 'avatars');
+
+CREATE POLICY "Avatar images are publicly accessible."
+  ON storage.objects
+  FOR SELECT
+  USING (bucket_id = 'avatars');
+
+CREATE POLICY "Anyone can upload an avatar."
+  ON storage.objects
+  FOR INSERT
+  WITH CHECK (bucket_id = 'avatars');
+
+grant delete on table "public"."profiles" to "anon";
+
+grant insert on table "public"."profiles" to "anon";
+
+grant references on table "public"."profiles" to "anon";
+
+grant select on table "public"."profiles" to "anon";
+
+grant trigger on table "public"."profiles" to "anon";
+
+grant truncate on table "public"."profiles" to "anon";
+
+grant update on table "public"."profiles" to "anon";
+
+grant delete on table "public"."profiles" to "authenticated";
+
+grant insert on table "public"."profiles" to "authenticated";
+
+grant references on table "public"."profiles" to "authenticated";
+
+grant select on table "public"."profiles" to "authenticated";
+
+grant trigger on table "public"."profiles" to "authenticated";
+
+grant truncate on table "public"."profiles" to "authenticated";
+
+grant update on table "public"."profiles" to "authenticated";
+
+grant delete on table "public"."profiles" to "service_role";
+
+grant insert on table "public"."profiles" to "service_role";
+
+grant references on table "public"."profiles" to "service_role";
+
+grant select on table "public"."profiles" to "service_role";
+
+grant trigger on table "public"."profiles" to "service_role";
+
+grant truncate on table "public"."profiles" to "service_role";
+
+grant update on table "public"."profiles" to "service_role";
+
+create policy "Public profiles are viewable by everyone."
+on "public"."profiles"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Users can insert their own profile."
+on "public"."profiles"
+as permissive
+for insert
+to public
+with check ((( SELECT auth.uid() AS uid) = id));
+
+
+create policy "Users can update own profile."
+on "public"."profiles"
+as permissive
+for update
+to public
+using ((( SELECT auth.uid() AS uid) = id));
+
+
+
+*/
